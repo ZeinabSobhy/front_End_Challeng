914cb47a69c09b9ee0991c3d2d52d2dc7aa7e9d8
zeinab sobhy <zeinab_sobhy_96@yahoo.com>
2021-07-12 01:36:38 +0200

    unit_test_button_clear

--- src/app.test.js
+++ src/app.test.js
@@ -7,4 +7,22 @@ test('side bar should be mounted', () => {
   const sideBarElement = queryByTitle("sidebar")
     expect(sideBarElement).toBeInTheDocument()
   })
+  test('when click clear text box should be empty', () => {
+    const {queryAllByRole}= render(<App/>)
+    const textBox = queryAllByRole('textbox')[0]
+    const clrBtn = queryAllByRole('button')[0]
+      textBox.nodeValue = "Product";
+      fireEvent.click(clrBtn)
+      expect(textBox.nodeValue).toBe(null)
+    })
+  
+  
+    test('when click clear text box should be empty', () => {
+      const {queryAllByRole}= render(<App/>)
+      const textBox = queryAllByRole('textbox')[1]
+      const clrBtn = queryAllByRole('button')[1]
+        textBox.nodeValue = "Cost";
+        fireEvent.click(clrBtn)
+        expect(textBox.nodeValue).toBe(null)
+      })
 
